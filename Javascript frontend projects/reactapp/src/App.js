ec919de1acef8d00f1d57144655ec7a4efe7ea21
Ayush Nanda <ananda_be18@thapar.edu>
2022-04-05 16:26:53 +0530

    conditionally outputting content and lists

--- Javascript frontend projects/reactapp/src/App.js
+++ Javascript frontend projects/reactapp/src/App.js
@@ -7,35 +7,44 @@ import Person from './Person/Person.js';
 class App extends Component{
    state={
     persons : [
-      { name: 'Max', age: 58},
-      { name: 'Manu', age: 29},
-      { name: 'Stephanie', age: 26}
+      { id: 'ergv', name: 'Max', age: 58},
+      { id: '3rc', name: 'Manu', age: 29},
+      { id: '42cac', name: 'Stephanie', age: 26}
     ],
     showPersons : false
   }
 
   //Event handler to manage switch
-  switchNameHandler = () => {
-    //console.log('Was clicked')
-    //Dont do this to change state this.state.persons[0].name = 'Maximillian'
-    this.setState({
-      persons : [
-        { name: 'Max', age: 58},
-        { name: 'Manu', age: 29},
-        { name: 'Stephanie', age: 27 }
-      ]
-     })
-  }
+  // switchNameHandler = () => {
+  //   //console.log('Was clicked')
+  //   //Dont do this to change state this.state.persons[0].name = 'Maximillian'
+  //   this.setState({
+  //     persons : [
+  //       { name: 'Max', age: 58},
+  //       { name: 'Manu', age: 29},
+  //       { name: 'Stephanie', age: 27 }
+  //     ]
+  //    })
+  // }
+
+  
 
   //on change Person.js // TWO_WAY BINDING
-  nameChangedHandler = (event) =>{
-    this.setState({
-      persons : [
-        { name: 'Max', age: 58},
-        { name: event.target.value, age: 29},
-        { name: 'Stephanie', age: 26 }
-      ]
-     })
+  nameChangedHandler = (event , id) =>{
+    const personIndex  =this.state.persons.findIndex(p =>{
+      return p.id === id;
+    });
+
+    const person  ={
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex]=  person;
+
+    this.setState( { persons : persons} )
   }
 
   //method for rendering some content conditionally
@@ -44,6 +53,12 @@ class App extends Component{
       this.setState({showPersons: !doesShow});
   }
 
+  deletePersonHandler = (personIndex)=>{
+    //const persons = this.state.persons.slice();
+    const persons  = [...this.state.persons]  //spread operator
+    persons.splice(personIndex , 1);
+    this.setState({persons : persons })
+  }
 
   render(){
 
@@ -53,33 +68,60 @@ class App extends Component{
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer'
+      
     };
+
+    let persons = null; 
+    if(this.state.showPersons){
+
+      persons=(
+       <div>
+
+        {/* -----------Rendering lists dynamically, mapping js-arrays into js-arrays but as jsx-objects---------------- */}
+
+        {this.state.persons.map( (person , index) =>{
+
+          return <Person   
+                          click={ ()=> this.deletePersonHandler(index)}
+                          name={person.name}
+                           age = {person.age}
+                          
+                           key ={person.id}
+                           changed ={ (event)=> this.nameChangedHandler(event, person.id)}
+                  />
+
+        })}
+
+
+
+
+        {/* -----------Tradtional method--------------------- */}
+        {/* <Person 
+          name={this.state.persons[0].name} age={this.state.persons[0].age}>
+        </Person> 
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click ={this.switchNameHandler}
+          changed={this.nameChangedHandler}>
+        </Person>
+        <Person 
+          name={this.state.persons[2].name} age={this.state.persons[2].age}>
+         </Person> */}
+        </div>  
+      );
+      }
+    
   return (
     <div className="App">
       <h1> Hi, I'm a react Developer </h1>
       <button 
       style={style} 
-      onClick={this.togglePersonsHandler}> Switch Name </button>
-      
-    {  this.state.showPersons ? 
-     <div>
-      <Person 
-        name={this.state.persons[0].name} age={this.state.persons[0].age}>
-      </Person> 
-      <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}
-        click ={this.switchNameHandler}
-        changed={this.nameChangedHandler}>
-      </Person>
-      <Person 
-        name={this.state.persons[2].name} age={this.state.persons[2].age}>
-       </Person>
-      </div>  
-
-      : null }
-
+      onClick={this.togglePersonsHandler}> Toogle Persons </button>
+ 
+      {persons}
     </div>
+  
   ); 
   }
 }
